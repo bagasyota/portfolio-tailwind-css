aa0cf3e0db0a8959e961104819184ebbb08bbb18
bagas <bagasyota7760@gmail.com>
2023-09-06 10:16:16 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#6366f1',
+        secondary: '#6b7280',
+        dark: '#0a0a0a',
+      }
+    },
+  },
+  plugins: [],
+}
+
